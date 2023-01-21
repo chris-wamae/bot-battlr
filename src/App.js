acf9928ccb0c244bdfadf13b2bf5f7d46a5cb0fb
chris-wamae <chriswamae123@gmail.com>
2023-01-21 05:54:21 +0300

    Installed axios API and tested it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [data,setData] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:3000/bots")
+    .then((data) => setData(data))
+  },[])
+console.log(data)
+
+
   return (
     <div className="App">
       <h1 className="text-5xl font-bold underline">
