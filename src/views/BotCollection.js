927a815fcc7ad9c398ec4d54a5c36b5f08d126b9
chris-wamae <chriswamae123@gmail.com>
2023-01-22 05:38:31 +0300

    Added bots to state to allow changing of page content

--- src/views/BotCollection.js
+++ src/views/BotCollection.js
@@ -1,56 +1,43 @@
-
 //BotSpecs should be rendered inside this component
 //when a bot card is clicked, its id is passed to the bot specs component
 //the bot specs component uses that id to filter that bot from the list
 //it renders that bot with additional information
 
-
 //Bot filter should be rendered inside this component
 
-
 //Sort Bar renders inside this component
-//this is a select menu with three options 
+//this is a select menu with three options
 //by health, by armor, by damage
 //if a sort option is selected, it sorts the bots i
-
 
 //All the Bot cards will be rendered here through map
 //if no classes are selected
 
-
-//if some classes are selected 
+//if some classes are selected
 //run a filter method before rendering
 //checkbox menu
 
-
-import React, { useEffect } from 'react';
-import { useState,useContext } from 'react';
-import BotCard from '../components/BotCard';
-import { BotContext} from '../data/BotsContext';
+import React, { useEffect } from "react";
+import { useState, useContext } from "react";
+import BotCard from "../components/BotCard";
+import { BotContext } from "../data/BotsContext";
 
 function BotCollection() {
-
-    const botsContext = useContext(BotContext)
-    const bots = botsContext.map((bot)=> {return <BotCard key={bot.id} bot={bot} showSpecs={showSpecs}/>}) 
-    const [specs,setSpecs] = useState([])
-    function showSpecs(singleBot){
-        setSpecs(<h3>{singleBot.name}</h3>)
-    }
- console.log(specs)
-    let render;
-    if(specs === []){
-       render = specs
-    }
-    else{
-        render = bots
-    }
-    console.log(render)
-    
-    return (
-        <div> 
-           {render}
-        </div>
+  const botsContext = useContext(BotContext);
+  const [bots, setBots] = useState([]);
+  console.log(botsContext)
+  useEffect(() =>{
+    setBots(botsContext.map((bot) => {
+        return <BotCard key={bot.id} bot={bot} showSpecs={showSpecs} />;
+      })
     );
+  },[botsContext])
+
+  function showSpecs(singleBot) {
+    setBots(<h3>{singleBot.name}</h3>);
+  }
+
+  return <div>{bots}</div>;
 }
 
 export default BotCollection;
