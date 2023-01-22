6147a1678c8a161c765ed65bbef74d682cb23c85
chris-wamae <chriswamae123@gmail.com>
2023-01-22 05:01:04 +0300

    Created BotCollection component that maps through all the bots and renders them using the BotCard component

--- src/views/BotCollection.js
+++ src/views/BotCollection.js
@@ -1,3 +1,4 @@
+
 //BotSpecs should be rendered inside this component
 //when a bot card is clicked, its id is passed to the bot specs component
 //the bot specs component uses that id to filter that bot from the list
@@ -21,3 +22,35 @@
 //run a filter method before rendering
 //checkbox menu
 
+
+import React, { useEffect } from 'react';
+import { useState,useContext } from 'react';
+import BotCard from '../components/BotCard';
+import { BotContext} from '../data/BotsContext';
+
+function BotCollection() {
+
+    const botsContext = useContext(BotContext)
+    const bots = botsContext.map((bot)=> {return <BotCard key={bot.id} bot={bot} showSpecs={showSpecs}/>}) 
+    const [specs,setSpecs] = useState([])
+    function showSpecs(singleBot){
+        setSpecs(<h3>{singleBot.name}</h3>)
+    }
+ console.log(specs)
+    let render;
+    if(specs === []){
+       render = specs
+    }
+    else{
+        render = bots
+    }
+    console.log(render)
+    
+    return (
+        <div> 
+           {render}
+        </div>
+    );
+}
+
+export default BotCollection;
